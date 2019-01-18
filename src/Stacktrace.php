f1e3c98ffcc2d298b0b0f593bc304e57b24131fa
Glendon Solsberry <gms8994@users.noreply.github.com>
2019-01-18 14:53:26 -0500

    Handles cases where the first line might not be the line that matches the regex (#4)
    * Handles cases where the first line might not be the line that matches the regex
    
    * Clean up per request

--- src/Stacktrace.php
+++ src/Stacktrace.php
@@ -137,7 +137,7 @@ class Stacktrace
             $newMessage = $newMessage[0];
             preg_match_all(static::REGEX_STACK_MESSAGE, $newMessage, $match);
 
-            $this->message = $match[1][0];
+            $this->message = $match[1][0] ?? $newMessage;
         }
 
         $this->brokenMap = array_map(function ($frame) {
