125abc34a97b8b1f202fe26604128489d53c8b7f
Austin Kregel <austin@kregel.co>
2021-03-19 23:53:23 -0400

    Fix the tests

--- tests/CodeframeTest.php
+++ tests/CodeframeTest.php
@@ -18,14 +18,14 @@ class CodeframeTest extends TestCase
     public function testWeCanGetTheFile()
     {
         $codeframe = new Codeframe('file', 10, ['line'], 'frame');
-        $this->assertSame('file', $codeframe->getFile());
+        self::assertSame('file', $codeframe->getFile());
     }
 
     public function testWeCanSetTheFile()
     {
         $codeframe = new Codeframe('file', 10, ['line'], 'frame');
         $codeframe->setFile('TestFile');
-        $this->assertNotEquals('file', $codeframe->getFile());
-        $this->assertEquals('TestFile', $codeframe->getFile());
+        self::assertNotEquals('file', $codeframe->getFile());
+        self::assertEquals('TestFile', $codeframe->getFile());
     }
 }
